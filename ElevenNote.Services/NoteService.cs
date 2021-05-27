d4dea53c8c5854500f2d5e87aa8a31d7b4d2ed96
sbarnes14 <sbarnes2015@gmail.com>
2021-05-27 12:59:43 -0400

    added GetNoteById

--- ElevenNote.Services/NoteService.cs
+++ ElevenNote.Services/NoteService.cs
@@ -52,5 +52,24 @@ namespace ElevenNote.Services
                 return query.ToArray();
             }
         }
+        public NoteDetail GetNoteById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Notes
+                        .Single(e => e.NoteId == id && e.OwnerId == _userId);
+                return
+                     new NoteDetail
+                     {
+                         NoteId = entity.NoteId,
+                         Title = entity.Title,
+                         Content = entity.Content,
+                         CreateUtc = entity.CreateUtc,
+                         ModifiedUtc = entity.ModifiedUtc
+                     };
+            }
+        }
     }
 }
